5f83e079d3d2e36d9cf4c0f191223895bf759ae5
HYSA <92003785+HYSAA@users.noreply.github.com>
2023-07-25 19:24:01 +0800

    Add files via upload

---
+++ shoot.js
@@ -0,0 +1,73 @@
+let res1=document.getElementById("outcome");
+let res2=document.getElementById("result2")
+let reset=document.getElementById("reset");
+let reset1=document.getElementById("reset2");
+let score=0;
+let score1=0;
+
+
+
+function add1() {
+
+    score+=1;
+    res1.innerText=score;
+    console.log(score)
+}
+function add2() {
+    score+=2;
+    res1.innerText=score;
+    console.log(score)
+}
+function add3() {
+    score+=3;
+    res1.innerText=score;
+    console.log(score)
+}
+function res() {
+res1.innerHTML=""
+res1.innerText=0;
+score=0;
+}
+
+
+///GUEST
+function add4() {
+   
+    score1+=1;
+    res2.innerText=score1;
+    console.log(score1)
+}
+function add5() {
+    score1+=2;
+    res2.innerText=score1;
+    console.log(score1)
+}
+function add6() {
+    score1+=3;
+    res2.innerText=score1;
+    console.log(score1)
+}
+function result() {
+res2.innerHTML=""
+res2.innerText=0;
+score1=0;
+}
+console.log("2" + 2) //2 and 2
+console.log(11 + 7) //18
+console.log(6 + "5") //6 5
+console.log("My points: " + 5 + 9) //My Points: 5 9
+console.log(2 + 2) // 2 2 
+console.log("11" + "14") // 25
+console.log("2" + 2) //2 and 2
+console.log(11 + 7) //18
+console.log(6 + "5") //6 5
+console.log("My points: " + 5 + 9) //My Points: 5 9
+console.log(2 + 2) // 2 2 
+console.log("11" + "14") // 25
+console.log("2" + 2) //2 and 2
+console.log(11 + 7) //18
+console.log(6 + "5") //6 5
+console.log("My points: " + 5 + 9) //My Points: 5 9
+console.log(2 + 2) // 2 2 
+console.log("11" + "14") // 25
+
